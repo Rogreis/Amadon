a9882795d781145fec08048aae1790432eff6e14
rogeriosilva <rogerio.silva@ctw.bmwgroup.com>
2024-06-20 13:57:55 +0100

    Iniciando projeto de instalacao

---
+++ AmadonInstall/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+using WixSharp;
+using WixToolset.Dtf.WindowsInstaller;
+using File = WixSharp.File;
+
+[assembly: InternalsVisibleTo(assemblyName: "AmadonInstall.aot")] // assembly name + '.aot suffix
+
+var project =
+    new ManagedProject("My Product",
+        new Dir(@"%ProgramFiles%\My Company\My Product",
+            new File("program.cs")),
+        new Property("PropName", "<your value>"));
+
+// project.AddUIProject("AmadonInstall.UI"); // name of the 'Custom UI Library' project in the solution
+
+project.Load += (e) =>
+{
+    Native.MessageBox("OnLoad", "WixSharp - .NET8");
+    e.Result = ActionResult.Failure;
+};
+
+project.BuildMsi();
